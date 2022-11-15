cf7e6afec81f002e7e61bff5f483039b106c850c
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-11-15 11:03:42 +0700

    not done

--- src/main/java/com/codegym/controller/StudentController.java
+++ src/main/java/com/codegym/controller/StudentController.java
@@ -1,22 +1,81 @@
 package com.codegym.controller;
 
+import com.codegym.model.Student;
+import com.codegym.service.student.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
 @Service
+@RequestMapping("/student")
 public class StudentController {
-//    @GetMapping("")
-//    public ModelAndView showStudent(Pageable pageable) {
-//        ModelAndView modelAndView = new ModelAndView("/student/list");
-//        Page<Blog> blogs;
-//        if (q.isPresent()) {
-//            blogs = blogService.findAllByDescriptionContaining(q.get(), pageable);
-//        } else {
-//            blogs = blogService.findAll(pageable);
-//        }
-//        modelAndView.addObject("blogs", blogs);
-//        return modelAndView;
-//    }
+    @Autowired
+    private IStudentService studentService;
+
+//    @Autowired
+//    private IClassroomService classroomService;
+
+    @GetMapping("")
+    public ModelAndView showStudent(Pageable pageable) {
+        ModelAndView modelAndView = new ModelAndView("student/list1");
+        Iterable<Student> studentPage = studentService.findAll();
+        modelAndView.addObject("students", studentPage);
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/student/create");
+        modelAndView.addObject("student", new Student());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView saveStudent(@ModelAttribute("student") Student student) {
+        ModelAndView modelAndView = new ModelAndView("/student/create");
+        studentService.save(student);
+        modelAndView.addObject("student", student);
+        modelAndView.addObject("message", "Student created");
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable("id") Long id) {
+        Optional<Student> student = studentService.findById(id);
+        if (!student.isPresent()) {
+            return new ModelAndView("/error-404");
+        } else {
+            return new ModelAndView("/student/edit", "blog", student.get());
+        }
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView editStudent(@ModelAttribute Student student) {
+        ModelAndView modelAndView = new ModelAndView("/student/edit");
+        studentService.save(student);
+        modelAndView.addObject("student", student);
+        modelAndView.addObject("message", "Student edited successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showDeleteForm(@PathVariable("id") Long id) {
+        Optional<Student> student = studentService.findById(id);
+        if (!student.isPresent()) {
+            return new ModelAndView("/error-404");
+        } else {
+            return new ModelAndView("/student/delete", "student", student.get());
+        }
+    }
+
+    @PostMapping("/delete")
+    public String deleteStudent(@ModelAttribute("student") Student student) {
+        studentService.remove(student.getId());
+        return "redirect:/student";
+    }
 }
